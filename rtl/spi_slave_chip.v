3b37d52e4ad798d987a6dade45153e0371a27c1a
Fuuraiko <p19y28t37h46o5n@gmail.com>
2026-08-20 00:08:20 +0800

    update IO

--- rtl/spi_slave_chip.v
+++ rtl/spi_slave_chip.v
@@ -16,17 +16,20 @@ module spi_slave_chip (
     inout  wire     pad_mosi,
     inout  wire     pad_miso
 );
-
+    wire POC;
     wire rst_n_i, ssn_i, sck_i, mosi_i;
     wire miso_o, miso_oen;
 
-    // ---- power pads (LVS only) ----
-    PVDD1CDG  u_pvdd1 (.VDD(VDD));
-    PVSS1CDG  u_pvss1 (.VSS(VSS));
-    PVDD2POC  u_poc   (.VDDPST(VDDPST));
-    PVSS2CDG  u_pvss2 (.VSSPST(VSSPST));
+    // ---- power pads ----
+    PVDD1CDG  u_pvdd1 (.VDD(VDD), .VSS(VSS), .VSSPST(VSSPST));
+    PVSS1CDG  u_pvss1 (.VSS(VSS), .VDDPST(VDDPST), .VSSPST(VSSPST));
+    PVDD2POC  u_poc   (.VDDPST(VDDPST), .VDD(VDD), .VSS(VSS), .VSSPST(VSSPST), .POC(POC));
+    PVSS2CDG  u_pvss2 (.VSSPST(VSSPST), .VDDPST(VDDPST));
 
     spi_slave u_core (
+        .VDD        (VDD),
+        .VSS        (VSS),
+
         .rst_n      (rst_n_i),
         .ssn        (ssn_i),
         .sck        (sck_i),
@@ -38,28 +41,33 @@ module spi_slave_chip (
     // ---- inputs ----
     PRUW0812SCDG u_pad_rst_n (
         .PAD (pad_rst_n), .C (rst_n_i),
-        .I   (1'b0), .OEN (1'b1), .IE (1'b1), .PE (1'b1), .DS (1'b1)
+        .I   (1'b0), .OEN (1'b1), .IE (1'b1), .PE (1'b1), .DS (1'b1),
+        .VDD (VDD), .VSS(VSS), .VDDPST(VDDPST), .VSSPST(VSSPST), .POC(POC)
     );
 
     PRUW0812SCDG u_pad_ssn (
         .PAD (pad_ssn), .C (ssn_i),
-        .I   (1'b0), .OEN (1'b1), .IE (1'b1), .PE (1'b1), .DS (1'b1)
+        .I   (1'b0), .OEN (1'b1), .IE (1'b1), .PE (1'b1), .DS (1'b1),
+        .VDD (VDD), .VSS(VSS), .VDDPST(VDDPST), .VSSPST(VSSPST), .POC(POC)
     );
 
     PRDW0812SCDG u_pad_sck (
         .PAD (pad_sck), .C (sck_i),
-        .I   (1'b0), .OEN (1'b1), .IE (1'b1), .PE (1'b1), .DS (1'b1)
+        .I   (1'b0), .OEN (1'b1), .IE (1'b1), .PE (1'b1), .DS (1'b1),
+        .VDD (VDD), .VSS(VSS), .VDDPST(VDDPST), .VSSPST(VSSPST), .POC(POC)
     );
 
     PRDW0812SCDG u_pad_mosi (
         .PAD (pad_mosi), .C (mosi_i),
-        .I   (1'b0), .OEN (1'b1), .IE (1'b1), .PE (1'b1), .DS (1'b1)
+        .I   (1'b0), .OEN (1'b1), .IE (1'b1), .PE (1'b1), .DS (1'b1),
+        .VDD (VDD), .VSS(VSS), .VDDPST(VDDPST), .VSSPST(VSSPST), .POC(POC)
     );
 
     // ---- miso, tri-state ----
     PRDW0812SCDG u_pad_miso (
         .PAD (pad_miso), .C (),
-        .I   (miso_o), .OEN (miso_oen), .IE (1'b0), .PE (1'b0), .DS (1'b0)
+        .I   (miso_o), .OEN (miso_oen), .IE (1'b0), .PE (1'b0), .DS (1'b0),
+        .VDD (VDD), .VSS(VSS), .VDDPST(VDDPST), .VSSPST(VSSPST), .POC(POC)
     );
 
 endmodule
